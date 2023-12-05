0fdf4fc9d3a4fcef716d354a3add222f8e8fc13a
Antonio <100844896+yellowminute@users.noreply.github.com>
2023-12-05 14:07:30 +0100

    Add a function to play 1 round

--- script.js
+++ script.js
@@ -4,4 +4,24 @@ function getComputerChoice() {                                          //Genera
     return choice;
 } 
 
-console.log(getComputerChoice());
+function playRound (playerSelection, computerSelection) {               //Play a round by comparing selections and based on them declare a winner
+    playerSelection = playerSelection.toLowerCase();
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if( playerSelection === computerSelection ) {
+        return "It's a tie!";
+    }
+    else if ( ( playerSelection === 'rock' && computerSelection === 'scissors' ) ||
+              ( playerSelection === 'paper' && computerSelection === 'rock' ) ||
+              ( playerSelection === 'scissors' && computerSelection === 'paper' ) ) {
+            return `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+
+}
+
+const playerSelection = 'ROck';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
