680121aa5bf68207b372cbd52b215374947fb84f
yellowminute <100844896+yellowminute@users.noreply.github.com>
2025-01-28 16:18:05 +0100

    Add playRound function
    function for one round of the game with variables for tracking score.

--- javascript.js
+++ javascript.js
@@ -18,10 +18,31 @@ function getHumanChoice () {
             return humanChoice;
         } 
         alert("Invalid choice!");
-  
     }
-     
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound (humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if((humanChoice === "paper" && computerChoice === "rock") || (humanChoice === "rock" && computerChoice === "scissors") || (humanChoice === "paper" && computerChoice === "rock")){
+        humanScore++;
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    }
+    else if(humanChoice === computerChoice) {
+        console.log(`You both picked ${humanChoice}! It's a draw.`);
+    }
+    else{
+        computerScore++;
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
