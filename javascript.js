00b95f3116556fb121f79bbd7355389e12277c19
yellowminute <100844896+yellowminute@users.noreply.github.com>
2025-01-28 15:18:22 +0100

    Add "computer" choice function randomly throw out one of the 3 options with switch statement.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,14 @@
-console.log("hello")
+function getComputerChoice () {
+    let computerChoice = Math.floor(Math.random()*3)
+    console.log(computerChoice);
+    switch (computerChoice) {
+        case 0: 
+        return "rock";
+        case 1:
+        return "paper";
+        case 2:
+        return "scissors";
+    }
+}
+
+console.log(getComputerChoice());
