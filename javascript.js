cea83c19595e3b943119d4a661684bd40059099a
yellowminute <100844896+yellowminute@users.noreply.github.com>
2025-01-31 21:22:48 +0100

    Add event listeners to buttons
    used forEach for listeners of button clicks.

--- javascript.js
+++ javascript.js
@@ -10,25 +10,12 @@ function getComputerChoice() {
     }
 }
 
-// function getHumanChoice() {
-//     while(true){
-//         let humanChoice = window.prompt("Pick one: rock, paper or scissors!");
-//         if(humanChoice.toLowerCase() === "rock" || 
-//             humanChoice.toLowerCase() === "paper" || 
-//             humanChoice.toLowerCase() === "scissors"){
-//             return humanChoice;
-//         } 
-//         alert("Invalid choice!");
-//     }
-// }
-
 function playRound(humanChoice, computerChoice) {
-    humanChoice = humanChoice.toLowerCase();
     console.log(humanChoice);
     console.log(computerChoice);
     if((humanChoice === "paper" && computerChoice === "rock") || 
         (humanChoice === "rock" && computerChoice === "scissors") || 
-        (humanChoice === "paper" && computerChoice === "rock")){
+        (humanChoice === "scissors" && computerChoice === "paper")){
         humanScore++;
         console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
     }
@@ -41,33 +28,24 @@ function playRound(humanChoice, computerChoice) {
     }
 }
 
-// function playGame() {
-//     /* for(i=0; i < 5; i++){
-//         const humanSelection = getHumanChoice();
-//         const computerSelection = getComputerChoice();
-//         playRound(humanSelection, computerSelection);
-//     } */
-//     if(humanScore > computerScore){
-//         console.log("Congratulations! You're the winner!");
-//     }
-//     else{
-//         console.log("Computer wins the game! Better luck next time!");
-//     }
-// }
+function playGame() {
+
+    let computerSelection = getComputerChoice();
+    const buttons = document.querySelectorAll("button");
+    buttons.forEach((button => {
+        button.addEventListener("click", () => {
+            playRound(button.id, computerSelection);
+        });
+    }));
+    // if(humanScore > computerScore){
+    //     console.log("Congratulations! You're the winner!");
+    // }
+    // else{
+    //     console.log("Computer wins the game! Better luck next time!");
+    // }
+}
 
 let humanScore = 0;
 let computerScore = 0;
 
-const computerSelection = getComputerChoice();
-
-const buttons = document.querySelectorAll("button");
-
-buttons.forEach((button => {
-    button.addEventListener("click", () => {
-        playRound(button.id, computerSelection);
-    });
-}));
-
-
-
-// playGame();
+playGame();
